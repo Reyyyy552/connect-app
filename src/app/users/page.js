fd7fee9c16312c35dad3a2934c3dc1b29dd2b62c
Reyyyy552 <raihanhidayat232@gmail.com>
2025-05-28 13:06:24 +0800

    components and looping

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,3 +1,5 @@
+import UserCard from "@/components/user-card";
+import { dataUser } from "@/mock/data-user";
 import {
   IconLogout2,
   IconUser,
@@ -6,6 +8,7 @@ import {
 } from "@tabler/icons-react";
 
 export default function UserPages() {
+  const data = dataUser;
   return (
     <div id="container" className="flex h-[100vh] text-black">
       <section
@@ -38,70 +41,15 @@ export default function UserPages() {
           className="w-full p-3 border rounded-lg mb-7 text-base"
         ></input>
         <div className="flex flex-col gap-4">
-          <div className="border rounded-lg p-2 flex justify-between items-center">
-            <div>
-              <h2 className="text-[18px] font-bold mb-[1px]">
-                Rahmat Saudi Al Fathir As
-              </h2>
-              <p className="text-base text-black mb-2">
-                rahmatsaudi@universitasmulia.ac.id
-              </p>
-              <span className="bg-black text-white text-[11px] px-[9px] py-[5px] rounded mr-[6px]">
-                Admin
-              </span>
-              <span className="bg-black text-white text-[11px] px-[9px] py-[5px] rounded">
-                Employee
-              </span>
-            </div>
-            <span className="bold-text status text-[16px] font-bold">
-              Aktif
-            </span>
-          </div>
-
-          <div className="border rounded-lg p-2 flex justify-between items-center">
-            <div>
-              <h2 className="text-[18px] font-bold mb-[1px]">Lintang</h2>
-              <p className="text-base text-black mb-2">
-                lintang@universitasmulia.ac.id
-              </p>
-              <span className="bg-black text-white text-[11px] px-[9px] py-[5px] rounded">
-                Employee
-              </span>
-            </div>
-            <span className="bold-text status text-[16px] font-bold">
-              Aktif
-            </span>
-          </div>
-
-          <div className="border rounded-lg p-2 flex justify-between items-center">
-            <div>
-              <h2 className="text-[18px] font-bold mb-[1px]">Shafira</h2>
-              <p className="text-base text-black mb-2">
-                shafira@universitasmulia.ac.id
-              </p>
-              <span className="bg-black text-white text-[11px] px-[9px] py-[5px] rounded">
-                Employee
-              </span>
-            </div>
-            <span className="bold-text status text-[16px] font-bold">
-              Aktif
-            </span>
-          </div>
-
-          <div className="border rounded-lg p-2 flex justify-between items-center">
-            <div>
-              <h2 className="text-[18px] font-bold mb-[1px]">Lebah Ganteng</h2>
-              <p className="text-base text-black mb-2">
-                lebahganteng@universitasmulia.ac.id
-              </p>
-              <span className="bg-black text-white text-[11px] px-[9px] py-[5px] rounded">
-                Employee
-              </span>
-            </div>
-            <span className="bold-text status text-[16px] font-bold">
-              Suspended
-            </span>
-          </div>
+          {data.map((employee, index) =>(
+            <UserCard
+            key={index}
+          fullname={employee.fullname}
+          email={employee.email}
+          role={employee.role}
+          status={employee.status}>
+          </UserCard>
+          ) )}
         </div>
       </section>
       <button className="fixed right-5 bottom-5 w-[42px] h-[42px] rounded-lg text-[26px] bg-[#e0e0e0] cursor-pointer">
---
+++ src/mock/data-user.js
@@ -0,0 +1,26 @@
+export const dataUser = [
+{
+    fullname: "Rahmad Saudi Al Fathir",
+    email:"rahmadsaudi@universitasmulia.ac.id",
+    role: "admin",
+    status:"aktif",
+},
+{
+    fullname: "Lintang",
+    email:"lintang@universitasmulia.ac.id",
+    role: "employee",
+    status:"aktif",
+},
+{
+    fullname: "Shafira",
+    email:"shafira@universitasmulia.ac.id",
+    role: "employee",
+    status:"aktif",
+},
+{
+    fullname: "Lebah Ganteng",
+    email:"lebahganteng@universitasmulia.ac.id",
+    role: "employee",
+    status:"suspended",
+},
+]
