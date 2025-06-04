7d104d81b1cdef72dfbc9727c0ed80117b847a8b
Reyyyy552 <raihanhidayat232@gmail.com>
2025-06-04 12:45:04 +0800

    roles and layout

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,16 +1,11 @@
-import UserCard from "@/components/user-card";
-import { dataUser } from "@/mock/data-user";
 import {
   IconLogout2,
   IconUser,
   IconUserBolt,
   IconUserCog,
-} from "@tabler/icons-react";
-
-export default function UserPages() {
-  const data = dataUser;
+} from "@tabler/icons-react";export default function AdminLayout({ children }) {
   return (
-    <div id="container" className="flex h-[100vh] text-black">
+     <div id="container" className="flex h-[100vh] text-black">
       <section
         id="navigation"
         className="bg-white p-10 w-[220px] border-r flex flex-col"
@@ -33,28 +28,11 @@ export default function UserPages() {
       </section>
       <section
         id="content"
-        className="bg-white w-[85%] flex-1 p-[30px] overflow-y-auto"
-      >
-        <input
-          type="search"
-          placeholder="Cari User"
-          className="w-full p-3 border rounded-lg mb-7 text-base"
-        ></input>
+        className="bg-white w-[85%] flex-1 p-[30px] overflow-y-auto">
         <div className="flex flex-col gap-4">
-          {data.map((employee, index) =>(
-            <UserCard
-            key={index}
-          fullname={employee.fullname}
-          email={employee.email}
-          role={employee.role}
-          status={employee.status}>
-          </UserCard>
-          ) )}
+            {children}
         </div>
       </section>
-      <button className="fixed right-5 bottom-5 w-[42px] h-[42px] rounded-lg text-[26px] bg-[#e0e0e0] cursor-pointer">
-        +
-      </button>
     </div>
   );
 }
---
+++ src/app/admin/users/page.js
@@ -0,0 +1,35 @@
+import UserCard from "@/components/user-card";
+import { dataUser } from "@/mock/data-user";
+
+
+export default function UserPages() {
+  const data = dataUser;
+  return (
+    <div id="container" className="flex h-[100vh] text-black"> 
+      <section
+        id="content"
+        className="bg-white w-[85%] flex-1 p-[30px] overflow-y-auto"
+      >
+        <input
+          type="search"
+          placeholder="Cari User"
+          className="w-full p-3 border rounded-lg mb-7 text-base"
+        ></input>
+        <div className="flex flex-col gap-4">
+          {data.map((employee, index) =>(
+            <UserCard
+            key={index}
+          fullname={employee.fullname}
+          email={employee.email}
+          role={employee.role}
+          status={employee.status}>
+          </UserCard>
+          ) )}
+        </div>
+      </section>
+      <button className="fixed right-5 bottom-5 w-[42px] h-[42px] rounded-lg text-[26px] bg-[#e0e0e0] cursor-pointer">
+        +
+      </button>
+    </div>
+  );
+}
