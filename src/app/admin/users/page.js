10451f1e2619477219918e527fe4ec20fa01ffb6
Reyyyy552 <raihanhidayat232@gmail.com>
2025-06-11 12:39:33 +0800

    jsonplaceholder

--- src/app/admin/users/page.js
+++ src/app/admin/users/page.js
@@ -1,11 +1,39 @@
+"use client";
+
 import UserCard from "@/components/user-card";
 import { dataUser } from "@/mock/data-user";
-
+import useSWR from "swr";
 
 export default function UserPages() {
   const data = dataUser;
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useSWR('https://jsonplaceholder.typicode.com/users', fetcher);
+
+  if (isLoading) {
+    return (
+      <div>
+        <p>Loading....</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>Gagal memuat data</p>
+      </div>
+    );
+  }
+
+  console.log(users);
+
   return (
-    <div id="container" className="flex h-[100vh] text-black"> 
+    <div id="container" className="flex h-[100vh] text-black">
       <section
         id="content"
         className="bg-white w-[85%] flex-1 p-[30px] overflow-y-auto"
@@ -16,15 +44,15 @@ export default function UserPages() {
           className="w-full p-3 border rounded-lg mb-7 text-base"
         ></input>
         <div className="flex flex-col gap-4">
-          {data.map((employee, index) =>(
+          {users.map((employee, index) => (
             <UserCard
-            key={index}
-          fullname={employee.fullname}
-          email={employee.email}
-          role={employee.role}
-          status={employee.status}>
-          </UserCard>
-          ) )}
+              key={index}
+              fullname={employee.name}
+              email={employee.email}
+              role={employee.phone}
+              status={employee.website}
+            ></UserCard>
+          ))}
         </div>
       </section>
       <button className="fixed right-5 bottom-5 w-[42px] h-[42px] rounded-lg text-[26px] bg-[#e0e0e0] cursor-pointer">
